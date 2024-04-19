173512ec27a5d62ac561113e0d9347f4c612469f
Relax-112 <mdiogocardoso@tutamail.com>
2024-04-19 12:10:03 -0700

    The Final Push
    Codigo do nosso website Front-End

--- Styles/script.js
+++ Styles/script.js
@@ -2,18 +2,18 @@
 
 // This is not a dar nao sei porque ver depois
 
-document.getElementById('password').addEventListener('input', function() {
-    var password = this.value;
-    var passwordPattern = /^(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-    var passwordMessage = document.getElementById('passwordMessage');
-    if (!passwordPattern.test(password)) {
-        passwordMessage.innerText = "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long";
-        this.classList.add('is-invalid');
-    } else {
-        passwordMessage.innerText = "";
-        this.classList.remove('is-invalid');
-    }
-});
+//document.getElementById('password').addEventListener('input', function() {
+  //  var password = this.value;
+ //   var passwordPattern = /^(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+   // var passwordMessage = document.getElementById('passwordMessage');
+   // if (!passwordPattern.test(password)) {
+   //     passwordMessage.innerText = "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long";
+   //     this.classList.add('is-invalid');
+   // } else {
+   //     passwordMessage.innerText = "";
+   //     this.classList.remove('is-invalid');
+   // }
+//});
 
 
 
@@ -22,8 +22,8 @@ document.getElementById('signup-form').addEventListener('submit', function(event
     var confirmPassword = document.getElementById('password2').value;
 
     if (password !== confirmPassword) {
-        event.preventDefault(); // Prevent form submission
-        document.getElementById('password-match-error').style.display = 'block'; // Show error message
+        event.preventDefault(); 
+        document.getElementById('password-match-error').style.display = 'block'; // erro
     }
 });
 
